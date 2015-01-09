7abf9bb9d1b36c34973ffeeb37093d8ca24bf8ba
Jacob Martella <jacob.martella@att.net>
2015-01-09 15:20:20 -0600

    Initial Commit
    First commit to the repo.

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+//* Start the engine
+include_once( get_template_directory() . '/lib/init.php' );
+
+//* Child theme (do not remove)
+define( 'CHILD_THEME_NAME', 'Faith Growth Starter Theme' );
+define( 'CHILD_THEME_URL', 'http://www.faithgrowth.com/' );
+define( 'CHILD_THEME_VERSION', '0.1' );
